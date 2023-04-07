957bd6696c7c4fa28934a70a7006d96dd38754e2
mandariny <mandariny716@gmail.com>
2023-04-07 17:56:20 +0900

    feat: python client 작성

--- python-client/main.py
+++ python-client/main.py
@@ -1,25 +1,29 @@
+# https://pypi.org/project/paho-mqtt/#usage-and-api
+
 import paho.mqtt.client as mqtt
 
-# The callback for when the client receives a CONNACK response from the server.
+mqtt = mqtt.Client(client_id="", clean_session=True, userdata=None, protocol="MQTTv311", transport="tcp")
+
+# connect 전 설정해야하는 옵션
+mqtt.enable_logger(logger=None)
+mqtt.user_data_set("데이터 전송!")
+
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("연결 성공!")
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print("연결 해제!")
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+def on_publish(client, userdata, mid):
+    print("In on_pub callback mid= ", mid)
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+mqtt.on_connect = on_connect
+mqtt.on_disconnect = on_disconnect
+mqtt.on_publish = on_publish
 
-client.connect("localhost", 1883, 60)
+mqtt.connect("localhost", port=1883, keepalive=60, bind_address="")
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+mqtt.publish("apple", payload=None, qos=0, retain=False)
+
+mqtt.disconnect()
